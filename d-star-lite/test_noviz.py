323116e70e9938f0121cb641d142d9a425546bd9
Technocracy <josiasmoukpe@gmail.com>
2021-11-16 09:08:52 -0500

    some progress for testing

--- d-star-lite/test_noviz.py
+++ d-star-lite/test_noviz.py
@@ -1,46 +1,34 @@
 # run profiling code for the project
 # import pygame
 from grid import GridWorld
-from utils import stateNameToCoords, parseDims
+from utils import stateNameToCoords, parseDims, add_reroute_obstacle
 from d_star_lite import init_dstarlite, move_and_rescan
 from brute_force import BruteForce
 # import random
 
-# path to design grid
-filep = "C:\\Users\\the_3\\Desktop\\AA\\testCaseGenerator\\data\\output.txt"
 
-# Initialize pygame
-# pygame.init()
-
-# X_DIM, Y_DIM = 32, 32
-X_DIM, Y_DIM = parseDims(filep) # reads dynamically the size of the grid
-VIEWING_RANGE = 10 # affect the ability of the algorithm to find it's path
-
-
-# Set title of screen
-# pygame.display.set_caption("D* Lite Path Planning")
-print("D* Lite Path Planning")
-
-# Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-# clock = pygame.time.Clock()
+#Testing parameters
+NUM_TRIALS = 6
+MAX_REROUTES = 9 # 0-9
+MAX_GRIDSIZE = 20
+MIN_GRIDSIZE = 5
 
 
 
-if __name__ == "__main__":
+def main(gridpath):
+
+    # X_DIM, Y_DIM = 32, 32
+    X_DIM, Y_DIM = parseDims(gridpath) # reads dynamically the size of the grid
+    VIEWING_RANGE = 10 # affect the ability of the algorithm to find it's path
+
+    print("D* Lite Path Planning")
+
+    # Loop until the user clicks the close button.
+    done = False
 
     # generate graph for the grid world
-    graph = GridWorld(X_DIM, Y_DIM, filepath=filep)
-    # s_start = 'x1y2' # [1, 2]
-
-    # generate starting point
-    # s_start = f'x{random.randint(0, X_DIM)}y{random.randint(0, Y_DIM)}'
-    # s_goal = 'x5y4'  # [5, 4]
-
-    # generate ending point
-    # s_goal = f'x{random.randint(0, X_DIM)}y{random.randint(0, Y_DIM)}'
+    graph = GridWorld(X_DIM, Y_DIM, filepath=gridpath)
+   
     s_start, s_goal = graph.start, graph.goal
     goal_coords = stateNameToCoords(s_goal)
 
@@ -51,7 +39,6 @@ if __name__ == "__main__":
     pqueue = []
 
     graph, pqueue, k_m = init_dstarlite(graph, pqueue, s_start, s_goal, k_m)
-
     s_current = s_start
     pos_coords = stateNameToCoords(s_current)
 
@@ -60,9 +47,12 @@ if __name__ == "__main__":
 
     # -------- Main Program Loop -----------
     while not done: 
-        # if continuous_run: 
-            # move the agent with new position to go to and new k_m | actual d-star lite pathfinding
+
+        # move the agent with new position to go to and new k_m | actual d-star lite pathfinding
         s_new, k_m = move_and_rescan(graph, pqueue, s_current, VIEWING_RANGE, k_m)
+        '''adding obstacles'''
+        if(add_reroute_obstacle(graph, s_new)):
+            rem_obstacles -= 1
 
         if s_new == 'goal':
             print('Goal Reached!')
@@ -74,5 +64,14 @@ if __name__ == "__main__":
             pos_coords = stateNameToCoords(s_current)
             # print('got pos coords: ', pos_coords)
 
-        '''adding obstacles'''
-        # add_obstacles()
+
+if __name__ == "__main__":
+    # path to design grid
+    path = "C:\\Users\\the_3\\Desktop\\AA\\testCaseGenerator\\data"
+
+    # file of increasing grid sizes
+    for gridsize in range():
+        for num_reroute in range()
+    main(path)
+   
+
